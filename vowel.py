afbb83fe5d7ed2ed66e59f30cb7226e1202195d2
subi44darsh <53809003+subi44darsh@users.noreply.github.com>
2019-08-06 13:06:06 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -5,4 +5,4 @@ if a in list:
 elif a=='a' or a=='e'or a=='i' or a=='o' or a=='u':
     print("Vowel")
 elif a!='a' and a!='e'and a!='i'and a!='o'and a!='u':
-    print("Consonants")
+    print("Consonant")
